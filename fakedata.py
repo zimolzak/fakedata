9762a4497415ab6d714c976bf0103687ca689575
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-11 16:51:11 -0400

    Make a header row. Verify that quoting works.

--- fakedata.py
+++ fakedata.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 import csv
+ncols = 6
 with open('eggs.csv', 'wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter = csv.writer(csvfile)
+    header =[]
+    for i in range(1,ncols+1):
+        header.append('col'+str(i))
+    spamwriter.writerow(header)
     spamwriter.writerow(['Spam']*5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam',
+                         'My parents, Ayn Rand and God', 'foo', 'bar'])
