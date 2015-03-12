9e898ed715aac1734a37788763bbb94f4e7b7ab6
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-12 14:28:44 -0400

    Generate correlated labs with defined error.

--- fakedata.py
+++ fakedata.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import csv
 import random
+import math
 # import schema # maybe later
 # import json # maybe later
 ncols = 6
@@ -14,11 +15,29 @@ with open('eggs.csv', 'wb') as csvfile:
     spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam',
                          'My parents, Ayn Rand and God', 'foo', 'bar'])
 
-for i in range(7):
-    print "Hemoglobin: " +\
-        str(round(random.normalvariate((14+17)/2,(14-17)/2),1))
+def three_sigs(x):
+    return round (x, 2-int(math.log10(x)))
 
 def fake_normal_lab(range_low, range_high, how_sick = 0):
-    return random.normalvariate((range_low+range_high)/2,
-                                (range_low-range_high)/2 * (how_sick +1)
-                                 )
+    assert 0 <= how_sick <= 1
+    return three_sigs (
+        random.normalvariate((range_low+range_high)/2,
+                             (range_low-range_high)/2 * (how_sick +1)
+                             )
+        )
+
+def correlate(x, slope, range_low, range_high, how_messy, intercept=0):
+    sigma_x = (range_high - range_low) / 2
+    epsilon = random.normalvariate(0, how_messy * sigma_x)
+    x = x + epsilon
+    return three_sigs(slope * x + intercept)
+
+hlow = 14
+hhigh = 17
+messy = 0.4 # around 0.4 is pretty good
+
+for i in range(7):
+    hgb = fake_normal_lab(hlow,hhigh,1)
+    hct = correlate(hgb, 3, hlow, hhigh, messy)
+    print "Hemoglobin: " + str(hgb) + " Hematocrit: " + str(hct) + \
+        " Ratio: " + str(three_sigs(hct / hgb))
