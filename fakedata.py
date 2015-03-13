2badccb4fdca3e936a4328882bc36eab5af74cf6
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-13 12:46:37 -0400

    Do not round early. Refactor some internals and magic nums.

--- fakedata.py
+++ fakedata.py
@@ -23,19 +23,20 @@ with open('eggs.csv', 'wb') as csvfile:
 
 def sigfig(x):
     number_of_figures = 3
-    return round (x, number_of_figures - 1 - int(math.log10(x)))
+    return str(round(x, number_of_figures - 1 - int(math.log10(x))))
 
 def fake_normal_lab(range_low, range_high, how_sick = 0):
     assert 0 <= how_sick <= 1 # sicker means wider standard dev.
     mu = (range_low + range_high) / 2
     sigma = (range_low - range_high) / 2 * (how_sick + 1)
-    return sigfig(random.normalvariate(mu, sigma))
+    return random.normalvariate(mu, sigma)
 
 def correlate(x, slope, range_low, range_high, how_messy, intercept=0):
-    sigma_x = (range_high - range_low) / 2
-    epsilon = random.normalvariate(0, how_messy * sigma_x)
-    x = x + epsilon
-    return sigfig(slope * x + intercept)
+    # to do: take in a function instead of (slope, intercept).
+    mu = 0
+    sigma = (range_high - range_low) / 2 * how_messy
+    epsilon = random.normalvariate(mu, sigma) # error to add to X.
+    return slope * (x + epsilon) + intercept
 
 def star_if_abnormal(x, range_low, range_high):
     if not (range_low <= x <= range_high):
@@ -46,20 +47,20 @@ def star_if_abnormal(x, range_low, range_high):
 hlow = 12 # hgb lower limit of normal. To do: check gender.
 hhigh = 17
 messy = 0.4 # higher means worse correlation. 0.4 is pretty good.
-t = datetime.date(2015,1,1)
 delta = 0.1 # brownian motion parameter
 morbidity_const = 0
+slope = 3 # relationship between hgb and hct
+avg_days = 90 # mean days between two lab measurements
 
-hgb = fake_normal_lab(hlow,hhigh,morbidity_const) # initial condition
+hgb = fake_normal_lab(hlow,hhigh,morbidity_const) # initial conditions
+t = datetime.date(2015,1,1)
 
 print "date\t\thgb\thct"
-
 for i in range(7):
-    hct = correlate(hgb, 3, hlow, hhigh, messy)
-    print str(t) + "\t" + str(hgb) + "\t" + str(hct) + \
+    hct = correlate(hgb, slope, hlow, hhigh, messy)
+    print str(t) + "\t" + sigfig(hgb) + "\t" + sigfig(hct) + \
         star_if_abnormal(hgb, hlow, hhigh)
-
-    # update rules
-    dt = datetime.timedelta(int(random.expovariate(1.0/90)))
+    # The update rules are below.
+    dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
     t = t + dt
-    hgb = sigfig(hgb + random.normalvariate(0, delta**2 * dt.days))
+    hgb = hgb + random.normalvariate(0, delta**2 * dt.days)
