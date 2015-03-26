769de5b8ec3a98097885e6eecdff59a3b86f2ab9
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-26 16:38:18 -0400

    remove global functions not needed

--- fakedata.py
+++ fakedata.py
@@ -8,30 +8,6 @@ import datetime
 # import schema # maybe later
 # import json # maybe later
 
-def sigfig(x, number_of_figures = 3):
-    return str(round(x, number_of_figures - 1 - int(math.log10(abs(x)))))
-
-def fake_normal_lab(range_low, range_high, how_sick = 0):
-    # As how_sick increases, that means wider standard dev.
-    assert 0 <= how_sick <= 1
-    mu = (range_low + range_high) / 2
-    sigma = (range_low - range_high) / 2 * (how_sick + 1)
-    return random.normalvariate(mu, sigma)
-
-def correlate(x, f, range_low, range_high, how_messy):
-    # Takes x, adds a little error to it, and outputs f(x). The last 3
-    # arguments determine how to add the error.
-    mu = 0
-    sigma = (range_high - range_low) / 2 * how_messy
-    epsilon = random.normalvariate(mu, sigma)
-    return f(x + epsilon)
-
-def star_if_abnormal(x, range_low, range_high):
-    if not (range_low <= x <= range_high):
-        return " **"
-    else:
-        return ""
-
 class LabDefinition:
     def __init__(self, labname, low, high, number_of_figures = 3):
         self.labname=labname
