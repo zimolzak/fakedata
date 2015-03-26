46446d9b223a4a5778671a70649c33f79942ea8f
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-26 16:37:19 -0400

    wrote new Class for a lab object

--- fakedata.py
+++ fakedata.py
@@ -32,9 +32,38 @@ def star_if_abnormal(x, range_low, range_high):
     else:
         return ""
 
+class LabDefinition:
+    def __init__(self, labname, low, high, number_of_figures = 3):
+        self.labname=labname
+        self.low=low
+        self.high=high
+    def set_random_initial(self, how_sick = 0):
+        assert 0 <= how_sick <= 1
+        mu = (self.low + self.high) / 2
+        sigma = (self.low - self.high) / 2 * (how_sick + 1)
+        self.value = random.normalvariate(mu, sigma)
+    def update(self, delta, dt):
+        # dt is a timedelta object
+        self.value = self.value + random.normalvariate(0, delta**2 * dt.days)
+    def correlate(self, f, how_messy):
+        # Takes x, adds a little error to it, and outputs f(x).
+        mu = 0
+        sigma = (self.high - self.low) / 2 * how_messy
+        epsilon = random.normalvariate(mu, sigma)
+        return self.sigfig(f(self.value + epsilon))
+    def sigfig(self, x, number_of_figures = 3):
+        return str(round(x, number_of_figures - 1 - int(math.log10(abs(x)))))
+    def __repr__(self):
+        return self.sigfig(self.value)
+    def star_if_abnormal(self):
+        if not (self.low <= self.value <= self.high):
+            return " **"
+        else:
+            return ""
+
 #### Parameters
-hlow = 12 # hgb lower limit of normal. To do: check gender.
-hhigh = 17
+hgb = LabDefinition("hgb", 12, 17)
+
 messy = 0.4 # higher means worse correlation.
 delta = 0.1 # brownian motion param, units lab/time^2. Hi=labile, lo=stable.
 morbidity_const = 0
@@ -42,17 +71,16 @@ avg_days = 90 # mean days between two lab measurements
 def hgb2hct(hgb):
     return 3 * hgb
 
-#### Initial conditions
-hgb = fake_normal_lab(hlow,hhigh,morbidity_const)
+### Initial conditions
+hgb.set_random_initial(morbidity_const)
 t = datetime.date(2015,1,1)
 
 labwriter = csv.writer(open('labs.csv', 'wb'))
 labwriter.writerow(["date", "hgb", "hct", "abnl"])
 for i in range(20):
-    hct = correlate(hgb, hgb2hct, hlow, hhigh, messy)
-    labwriter.writerow([str(t), sigfig(hgb), sigfig(hct),
-                        star_if_abnormal(hgb, hlow, hhigh)])
+    hct = hgb.correlate(hgb2hct, messy)
+    labwriter.writerow([str(t), hgb, hct, hgb.star_if_abnormal()])
     # The update rules are below.
     dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
     t = t + dt
-    hgb = hgb + random.normalvariate(0, delta**2 * dt.days)
+    hgb.update(delta,dt)
