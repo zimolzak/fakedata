6920d22848fc3eefbde196138638d39608c21b51
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-27 13:42:47 -0400

    mechanism to add and update a new concept called a Correlate

--- fakedata.py
+++ fakedata.py
@@ -13,16 +13,29 @@ class LabDefinition:
         self.labname=labname
         self.low=low
         self.high=high
-        self.set_random_initial(0)
-    def set_random_initial(self, how_sick = 0):
+        self.reset_main(0)
+    def reset_main(self, how_sick = 0):
         assert 0 <= how_sick <= 1
         mu = (self.low + self.high) / 2
         sigma = (self.low - self.high) / 2 * (how_sick + 1)
         self.value = random.normalvariate(mu, sigma)
-        self.correlates = {}
+        self.correlate_values = {}
+        self.correlate_functions = {}
     def update(self, delta, dt):
         # dt is a timedelta object
         self.value = self.value + random.normalvariate(0, delta**2 * dt.days)
+        for k in self.correlate_functions.keys():
+            self.reset_correlate(k)
+    def new_correlate(self, name, f, how_messy):
+        self.correlate_functions[name] = {'function':f, 'messy':how_messy}
+        self.reset_correlate(name)
+    def reset_correlate(self, name):
+        mu = 0
+        f = self.correlate_functions[name]['function']
+        how_messy = self.correlate_functions[name]['messy']
+        sigma = (self.high - self.low) / 2 * how_messy
+        epsilon = random.normalvariate(mu, sigma)
+        self.correlate_values[name] = f(self.value + epsilon)
     def correlate(self, f, how_messy):
         # Takes x, adds a little error to it, and outputs f(x).
         mu = 0
@@ -48,13 +61,15 @@ avg_days = 90 # mean days between two lab measurements
 def hgb2hct(hgb):
     return 3 * hgb
 
+hgb.new_correlate('hct', hgb2hct, messy)
+
 ### Initial conditions
 t = datetime.date(2015,1,1)
 
 labwriter = csv.writer(open('labs.csv', 'wb'))
 labwriter.writerow(["date", "hgb", "hct", "abnl"])
 for i in range(20):
-    hct = hgb.correlate(hgb2hct, messy)
+    hct = hgb.correlate_values['hct']
     labwriter.writerow([str(t), hgb, hct, hgb.star_if_abnormal()])
     # The update rules are below.
     dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
