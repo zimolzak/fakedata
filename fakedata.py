90b1d36379fa8873e5cb93df708b904c95c10514
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-27 14:25:14 -0400

    support for multiple so-called Root labs

--- fakedata.py
+++ fakedata.py
@@ -9,43 +9,39 @@ import datetime
 # import json # maybe later
 
 class LabDefinition:
-    def __init__(self, labname, low, high, number_of_figures = 3):
-        self.labname=labname
-        self.low=low
-        self.high=high
-        self.reset_main(0)
-    def reset_main(self, how_sick = 0):
+    def __init__(self, labname, low, high):
+        self.roots = {labname: {'low':low, 'high':high} }
+        self.reset_roots(0)
+    def reset_roots(self, how_sick = 0): #may want this public
         assert 0 <= how_sick <= 1
-        mu = (self.low + self.high) / 2
-        sigma = (self.low - self.high) / 2 * (how_sick + 1)
-        self.value = random.normalvariate(mu, sigma)
+        for k, v in self.roots.iteritems():
+            mu = (v['low'] + v['high']) / 2
+            sigma = (v['low'] - v['high']) / 2 * (how_sick + 1)
+            self.roots[k]['value'] = random.normalvariate(mu, sigma)
         self.correlate_values = {}
         self.correlate_functions = {}
     def update(self, delta, dt):
         # dt is a timedelta object
-        self.value = self.value + random.normalvariate(0, delta**2 * dt.days)
+        for k in self.roots.keys():
+            self.roots[k]['value'] = self.roots[k]['value'] + random.normalvariate(0, delta**2 * dt.days)
         for k in self.correlate_functions.keys():
             self.reset_correlate(k)
-    def new_correlate(self, name, f, how_messy):
-        self.correlate_functions[name] = {'function':f, 'messy':how_messy}
+    def new_correlate(self, name, f, rootname, how_messy):
+        self.correlate_functions[name] = {'function':f, 'rootname':rootname, 'messy':how_messy}
         self.reset_correlate(name)
     def reset_correlate(self, name):
         # Takes main value, adds a little error to it, and stores f(x).
         mu = 0
         f = self.correlate_functions[name]['function']
         how_messy = self.correlate_functions[name]['messy']
-        sigma = (self.high - self.low) / 2 * how_messy
+        myroot = self.correlate_functions[name]['rootname']
+        sigma = (self.roots[myroot]['high'] - self.roots[myroot]['low']) / 2 * how_messy
         epsilon = random.normalvariate(mu, sigma)
-        self.correlate_values[name] = self.sigfig(f(self.value + epsilon))
+        self.correlate_values[name] = self.sigfig(f(self.roots[myroot]['value'] + epsilon))
+    def new_root(self, name, low, high):
+        pass #FIXME
     def sigfig(self, x, number_of_figures = 3):
         return str(round(x, number_of_figures - 1 - int(math.log10(abs(x)))))
-    def __repr__(self):
-        return self.sigfig(self.value)
-    def star_if_abnormal(self):
-        if not (self.low <= self.value <= self.high):
-            return " **"
-        else:
-            return ""
 
 #### Parameters
 messy = 0.4 # higher means worse correlation.
@@ -55,16 +51,17 @@ def hgb2hct(hgb):
     return 3 * hgb
 
 ### Initial conditions
-hgb = LabDefinition("hgb", 12, 17)
-hgb.new_correlate('hct', hgb2hct, messy)
+cbc = LabDefinition("hgb", 12, 17)
+cbc.new_correlate('hct', hgb2hct, 'hgb', messy)
 t = datetime.date(2015,1,1)
 
 labwriter = csv.writer(open('labs.csv', 'wb'))
-labwriter.writerow(["date", "hgb", "hct", "abnl"])
+labwriter.writerow(["date", "hgb", "hct"])
 for i in range(20):
-    hct = hgb.correlate_values['hct']
-    labwriter.writerow([str(t), hgb, hct, hgb.star_if_abnormal()])
+    hgb = cbc.roots['hgb']['value']
+    hct = cbc.correlate_values['hct']
+    labwriter.writerow([str(t), hgb, hct])
     # The update rules are below.
     dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
     t = t + dt
-    hgb.update(delta,dt)
+    cbc.update(delta,dt)
