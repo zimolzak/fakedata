80b160bf0a09ed9d52c089c2164f80b1666449bc
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-03-30 16:45:46 -0400

    date of birth

--- names.py
+++ names.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 import random
+import datetime
 
 Proportion_Male = 0.927
 Age_ranges = [18, 35, 55, 65, 75, 91]
 Age_cumu_p = {"M":[0.071, 0.306, 0.541, 0.765, 1.1], \
                      "F":[0.210, 0.667, 0.834, 0.903, 1.1]}
+
+# source: American FactFinder, factfinder.census.gov
+
+# Sex by age by veteran status for the civilian population 18 years
+# and over. 2009-2013 american community survey 5-year estimates.
+# ACS_13_5YR_B21001. By census 5-digit ZIP code tabulation area
+# (ZCTA5).
 
 gender = ""
 if random.uniform(0,1) > Proportion_Male:
@@ -23,7 +31,8 @@ for i, cutpoint in enumerate(Age_cumu_p[gender]):
         age = random.randint(Age_ranges[i], Age_ranges[i+1] - 1)
         break
 
-
+extradays = random.randint(0,364)
+dob = datetime.date(2015, 3, 30) - datetime.timedelta(age * 365 + extradays)
 
 full_name = []
 for filename in ('dist.all.last', firstname):
@@ -52,4 +61,5 @@ for filename in ('dist.all.last', firstname):
 
 full_name.append(gender)
 full_name.append(age)
+full_name.append(dob)
 print full_name
