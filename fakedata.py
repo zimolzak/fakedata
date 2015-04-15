19467451227b4b1614364a229ec8b4c0ba67e350
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-15 15:47:44 -0400

    support for multiple patients, each with series of labs

--- fakedata.py
+++ fakedata.py
@@ -11,21 +11,28 @@ from lab import *
 #### Parameters
 delta = 0.03 # brownian motion param, units 1/time^2. Hi=labile, lo=stable.
 avg_days = 90 # mean days between two lab measurements
+labs_per_patient = 20
+patients_to_generate = 3
 
-### Initial conditions
-Panel = CbcBmp()
-t = datetime.date(2014,1,1) + datetime.timedelta(random.randint(0,365))
-
-### Loop and write
+#### Write
+test_panel = CbcBmp()
 labwriter = csv.writer(open('labs.csv', 'wb'))
-keys_ordered = Panel.contents().keys()
-labwriter.writerow(["date"] + keys_ordered)
-for i in range(20):
-    vals_ordered = []
-    for k in keys_ordered:
-        vals_ordered.append(Panel.contents(True)[k])
-    labwriter.writerow([str(t)] + vals_ordered)
-    # The update rules are below.
-    dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
-    t = t + dt
-    Panel.update(delta,dt)
+keys_ordered = test_panel.contents().keys()
+labwriter.writerow(["id", "date"] + keys_ordered)
+
+for id in range(patients_to_generate):
+
+    ### Initial conditions
+    Panel = CbcBmp()
+    t = datetime.date(2014,1,1) + datetime.timedelta(random.randint(0,365))
+
+    ### Loop and write
+    for i in range(labs_per_patient):
+        vals_ordered = []
+        for k in keys_ordered:
+            vals_ordered.append(Panel.contents(True)[k])
+        labwriter.writerow([id, str(t)] + vals_ordered)
+        # The update rules are below.
+        dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
+        t = t + dt
+        Panel.update(delta,dt)
