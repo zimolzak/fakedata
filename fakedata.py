28b4cd0ef29dace6bc204cdb407852f79821755f
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-15 16:05:42 -0400

    Use the Patient class in main file and write a new CSV with those patient demographics.

--- fakedata.py
+++ fakedata.py
@@ -5,6 +5,7 @@ import csv
 import random
 import datetime
 from lab import *
+from names import *
 # import schema # maybe later
 # import json # maybe later
 
@@ -14,19 +15,33 @@ avg_days = 90 # mean days between two lab measurements
 labs_per_patient = 20
 patients_to_generate = 3
 
-#### Write
+#### Initialize CSV files with headers
 test_panel = CbcBmp()
 labwriter = csv.writer(open('labs.csv', 'wb'))
 keys_ordered = test_panel.contents().keys()
 labwriter.writerow(["id", "date"] + keys_ordered)
 
+test_patient = Patient()
+patientwriter = csv.writer(open('patients.csv', 'wb'))
+patient_keys_ordered = test_patient.__dict__.keys()
+patientwriter.writerow(["id"] + patient_keys_ordered)
+
+#### Generate patients
+
 for id in range(patients_to_generate):
 
     ### Initial conditions
+    current_person = Patient()
     Panel = CbcBmp()
     t = datetime.date(2014,1,1) + datetime.timedelta(random.randint(0,365))
 
-    ### Loop and write
+    ### Write to patient table
+    vals_ordered = []
+    for k in patient_keys_ordered:
+        vals_ordered.append(current_person.__dict__[k])
+    patientwriter.writerow([id] + vals_ordered)
+
+    ### Loop and write to lab table
     for i in range(labs_per_patient):
         vals_ordered = []
         for k in keys_ordered:
