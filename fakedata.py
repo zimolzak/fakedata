da2561eba51e01f7c13e1b3ec6fdd804cecf67dd
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-15 15:39:07 -0400

    use the new CbcBmp class, thus simplifying main file a lot

--- fakedata.py
+++ fakedata.py
@@ -13,40 +13,7 @@ delta = 0.03 # brownian motion param, units 1/time^2. Hi=labile, lo=stable.
 avg_days = 90 # mean days between two lab measurements
 
 ### Initial conditions
-Panel = LabDefinition()
-
-for file in ["bmp_ranges.csv", "cbc_ranges.csv"]:
-    lines = csv.reader(open(file, 'r'), delimiter=',')
-    for fields in lines:
-        Panel.new_root(fields[0], *map(float,fields[1:]))
-
-def cl_func(na, ag, hco3):
-    return na - ag - hco3
-def hgb2hct(hgb):
-    return 3 * hgb
-def mchc_func(hgb, hct):
-    return hgb / (hct / 100)
-def rbc_func(hct, mcv):
-    return 10 * hct / mcv
-def mch_func(hgb, rbc):
-    return 10 * hgb / rbc
-def neut2lymph(neut):
-    return random.uniform(0.66,0.72) * (100 - neut)
-def mono_func(n,l,e,b):
-    m = 100 - n - l - e - b
-    if m > 0:
-        return m
-    else:
-        return 0
-
-Panel.new_correlate('cl', cl_func, ['na', 'ag', 'hco3'])
-Panel.new_correlate('hct', hgb2hct, ['hgb'], how_messy=0.3)
-Panel.new_correlate('rbc', rbc_func, ['hct', 'mcv']) # must be after hct
-Panel.new_correlate('mchc', mchc_func, ['hgb', 'hct']) # must be after hct
-Panel.new_correlate('mch', mch_func, ['hgb', 'rbc']) # must be after rbc
-Panel.new_correlate('lymph', neut2lymph, ['neut'])
-Panel.new_correlate('mono', mono_func, ['neut', 'lymph', 'eos', 'baso'])
-
+Panel = CbcBmp()
 t = datetime.date(2014,1,1) + datetime.timedelta(random.randint(0,365))
 
 ### Loop and write
