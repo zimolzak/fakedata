49ee609e339ffa819c3812c4b79c180bc0f51f73
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-15 15:12:01 -0400

    Roll the demographics code into a class.

--- names.py
+++ names.py
@@ -3,106 +3,125 @@ import random
 import datetime
 import csv
 
-Proportion_Male = 0.927
-Age_ranges = [18, 35, 55, 65, 75, 91]
-Age_cumu_p = {"M":[0.071, 0.306, 0.541, 0.765, 1.1], \
-                     "F":[0.210, 0.667, 0.834, 0.903, 1.1]}
+class Patient:
+    def __init__(self):
+        Proportion_male = 0.927
+        Age_ranges = [18, 35, 55, 65, 75, 91]
+        Age_cumu_p = {"M":[0.071, 0.306, 0.541, 0.765, 1.1], \
+                               "F":[0.210, 0.667, 0.834, 0.903, 1.1]}
 
-# source: American FactFinder, factfinder.census.gov
-
-# Sex by age by veteran status for the civilian population 18 years
-# and over. 2009-2013 american community survey 5-year estimates.
-# ACS_13_5YR_B21001. By census 5-digit ZIP code tabulation area
-# (ZCTA5).
-
-gender = ""
-if random.uniform(0,1) > Proportion_Male:
-    firstname_file = 'dist.female.first'
-    gender = "F"
-else:
-    firstname_file = 'dist.male.first'
-    gender = "M"
-
-age = None
-age_quantile = random.uniform(0,1)
-for i, cutpoint in enumerate(Age_cumu_p[gender]):
-    if age_quantile > cutpoint:
-        continue
-    else:
-        age = random.randint(Age_ranges[i], Age_ranges[i+1] - 1)
-        break
-extradays = random.randint(0,364)
-dob = datetime.date(2015, 3, 30) - datetime.timedelta(age * 365 + extradays)
-
-def quantile2text(quantile, filename, q_column, t_column, split_text):
-    file = open(filename, 'r')
-    found_name = ""
-    names_to_pick = []
-    matching_proportion = 0
-    while found_name == "":
-        fields = file.readline().split(split_text)
-        name = fields[t_column]
-        cumu_p = float(fields[q_column])
-        if quantile <= cumu_p and matching_proportion == 0:
-            names_to_pick.append(name)
-            matching_proportion = cumu_p
-            continue
-        elif quantile <= cumu_p and matching_proportion == cumu_p:
-            names_to_pick.append(name)
-            continue
-        elif quantile > cumu_p:
-            continue
+        # source: American FactFinder, factfinder.census.gov. Sex by
+        # age by veteran status for the civilian population 18 years
+        # and over. 2009-2013 american community survey 5-year
+        # estimates. ACS_13_5YR_B21001. By census 5-digit ZIP code
+        # tabulation area (ZCTA5).
+        
+        ######## Gender ########
+        
+        self.gender = ""
+        if random.uniform(0,1) > Proportion_male:
+            firstname_file = 'dist.female.first'
+            self.gender = "F"
         else:
-            assert quantile <= cumu_p and matching_proportion < cumu_p
-            found_name = random.choice(names_to_pick).capitalize()
-    return found_name
+            firstname_file = 'dist.male.first'
+            self.gender = "M"
 
-demographics = []
-for filename in ('dist.all.last', firstname_file):
-    r = random.uniform(0, 90.483)
-    demographics.append(quantile2text(r, filename, 2, 0, None) + "_fake")
+        ######## Age and DOB ########
+        
+        self.age = None
+        age_quantile = random.uniform(0,1)
+        for i, cutpoint in enumerate(Age_cumu_p[self.gender]):
+            if age_quantile > cutpoint:
+                continue
+            else:
+                self.age = random.randint(Age_ranges[i],
+                                          Age_ranges[i+1] - 1)
+                break
+        extradays = random.randint(0,364)
+        self.dob = datetime.date(2015, 3, 30) - \
+            datetime.timedelta(self.age * 365 + extradays)
 
-zip = quantile2text(random.uniform(0,1), 'zipcodes.csv', 2, 0, ",").split()[1]
+        ######## Name ########
+        
+        def quantile2text(quantile, filename, q_column, t_column, split_text):
+            file = open(filename, 'r')
+            found_name = ""
+            names_to_pick = []
+            matching_proportion = 0
+            while found_name == "":
+                fields = file.readline().split(split_text)
+                name = fields[t_column]
+                cumu_p = float(fields[q_column])
+                if quantile <= cumu_p and matching_proportion == 0:
+                    names_to_pick.append(name)
+                    matching_proportion = cumu_p
+                    continue
+                elif quantile <= cumu_p and matching_proportion == cumu_p:
+                    names_to_pick.append(name)
+                    continue
+                elif quantile > cumu_p:
+                    continue
+                else:
+                    assert quantile <= cumu_p and matching_proportion < cumu_p
+                    found_name = random.choice(names_to_pick).capitalize()
+            return found_name
 
-# source: http://www.unitedstateszipcodes.org/zip-code-database/
-zipcode_iterator = csv.reader(open("zip_code_database.csv", 'r'),
-                              delimiter=',', quotechar='"')
+        self.fullname = []
+        for filename in ('dist.all.last', firstname_file):
+            r = random.uniform(0, 90.483)
+            self.fullname.append(quantile2text(r, filename, 2, 0, None) \
+                                     + "_fake")
 
-city = ""
-state = ""
-for fields in zipcode_iterator:
-    if zip != fields[0]:
-        continue
-    else:
-        city = fields[2]
-        state = fields[5]
-        break
+        ######## ZIP, City, State ########
+        
+        self.zip = quantile2text(random.uniform(0,1),
+                                 'zipcodes.csv', 2, 0, ",").split()[1]
+        # source: http://www.unitedstateszipcodes.org/zip-code-database/
 
-addr = str(random.randint(10,9999)) + " " + random.choice(["Elm", "Pine", "Maple", "State", "Main"]) + " " + random.choice(["St", "Ln", "Blvd"])
+        zipcode_iterator = csv.reader(open("zip_code_database.csv", 'r'),
+                                      delimiter=',', quotechar='"')
 
-phone = ""
-fallback_npa = ""
-npa_iterator = csv.reader(open("npa_city_state.csv", 'r'),
-                              delimiter=',', quotechar='"')
-for npa, test_city, test_state in npa_iterator:
-    if test_state == state:
-        fallback_npa = str(npa)
-    if test_state == state and test_city == city:
-        phone = str(npa) + "-555-" + str(random.randint(0,9)) \
-            + str(random.randint(0,9)) + str(random.randint(0,9)) \
-            + str(random.randint(0,9))
-        break
-if phone == "":
-    phone = fallback_npa + "-555-" + str(random.randint(0,9)) \
-        + str(random.randint(0,9)) + str(random.randint(0,9)) \
-        + str(random.randint(0,9))
+        self.city = ""
+        self.state = ""
+        for fields in zipcode_iterator:
+            if self.zip != fields[0]:
+                continue
+            else:
+                self.city = fields[2]
+                self.state = fields[5]
+                break
 
-demographics.append(gender)
-demographics.append(age)
-demographics.append(dob)
-demographics.append(addr)
-demographics.append(city)
-demographics.append(state)
-demographics.append(zip)
-demographics.append(phone)
-print demographics
+        ######## Address ########
+        
+        self.addr = str(random.randint(10,9999)) + " " + \
+            random.choice(["Elm", "Pine", "Maple", "State", "Main"]) + " " + \
+            random.choice(["St", "Ln", "Blvd"])
+
+        ######## Phone ########
+        
+        self.phone = ""
+        fallback_npa = ""
+        npa_iterator = csv.reader(open("npa_city_state.csv", 'r'),
+                                  delimiter=',', quotechar='"')
+        for npa, test_city, test_state in npa_iterator:
+            if test_state == self.state:
+                fallback_npa = str(npa)
+            if test_state == self.state and test_city == self.city:
+                self.phone = str(npa) + "-555-" + str(random.randint(0,9)) \
+                    + str(random.randint(0,9)) + str(random.randint(0,9)) \
+                    + str(random.randint(0,9))
+                break
+        if self.phone == "":
+            self.phone = fallback_npa + "-555-" + str(random.randint(0,9)) \
+                + str(random.randint(0,9)) + str(random.randint(0,9)) \
+                + str(random.randint(0,9))
+
+# demographics.append(self.gender)
+# demographics.append(self.age)
+# demographics.append(self.dob)
+# demographics.append(addr)
+# demographics.append(city)
+# demographics.append(state)
+# demographics.append(zip)
+# demographics.append(phone)
+# print demographics
