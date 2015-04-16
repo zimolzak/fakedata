24a6dbb24af44555a0309f50a79cea427b6fe9fd
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-16 12:53:05 -0400

    Script allowing subset of input files to show their structure

---
+++ make_sample_datasets.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+head dist.all.last         > ./sample_datasets/dist.all.last
+head dist.female.first     > ./sample_datasets/dist.female.first
+head dist.male.first       > ./sample_datasets/dist.male.first
+head npanxx99.txt          > ./sample_datasets/npanxx99.txt
+head zip_code_database.csv > ./sample_datasets/zip_code_database.csv
+head zipcodes.csv          > ./sample_datasets/zipcodes.csv
