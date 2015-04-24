89e1851e88239925fae90188264f9fee61ced1ea
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-24 16:17:17 -0400

    Account for genes that can be altered in more than one way.

--- genes.py
+++ genes.py
@@ -32,6 +32,10 @@ if histology != 'Large-cell':
 
 gene_set = set(['ABL1', 'ERBB4', 'GNAQ', 'MTOR', 'RET', 'AKT1', 'EZH2', 'GNAS', 'NF1', 'ROS1', 'ALK', 'FANCA', 'HNF1A', 'NF2', 'SMAD4', 'APC', 'FANCC', 'HRAS', 'NOTCH1', 'SMARCB1', 'ATM', 'FANCD2', 'IDH1', 'NPM1', 'SMO', 'BRAF', 'FANCE', 'IDH2', 'NRAS', 'SRC', 'BRCA1', 'FANCF', 'JAK2', 'NTRK1', 'STK11', 'BRCA2', 'FANCG', 'JAK3', 'PALB2', 'TERT', 'BRIP1', 'FANCL', 'KDR', 'PDGFRA', 'TP53', 'CDH1', 'FBXW7', 'KIT', 'PDGFRB', 'TSC1', 'CDKN2A', 'FGFR1', 'KRAS', 'PIK3CA', 'TSC2', 'CSF1R', 'FGFR2', 'MET', 'PMS2', 'VHL', 'CTNNB1', 'FGFR3', 'MLH1', 'PTCH1', 'DDR2', 'FLT3', 'MPL', 'PTEN', 'EGFR', 'FOXL2', 'MSH2', 'PTPN11', 'ERBB2', 'GNA11', 'MSH6', 'RB1', 'ERBB3 amplification', 'MYC amplification', 'MYCN amplification', 'ETV6 rearrangement', 'ETV1 rearrangement', 'EWSR1 rearrangement', 'TMPRSS2 rearrangement', 'BCL2 rearrangement', 'ETV4 rearrangement', 'MLL rearrangement', 'RARA rearrangement'])
 
+cnv = ['ALK', 'FGFR3', 'RET', 'EGFR', 'FGFR1', 'KIT', 'ERBB2', 'FGFR2', 'MET', 'PDGFRA']
+
+rearr = ['ABL1', 'EGFR', 'PDGFRA', 'ROS1', 'ALK', 'PDGFRB']
+
 if histology != 'Large-cell':
      for gene, frequency in gene_freq[histology].iteritems():
          if frequency > 0.0:
@@ -54,7 +58,13 @@ if how_many_more > 0:
     genes_for_this_hist = genes_for_this_hist + \
         sample_about_how_many(gene_set, how_many_more)
 
-
-
+for i, gene in enumerate(genes_for_this_hist):
+    potential_alterations = ['']
+    if gene in cnv:
+        potential_alterations.append(' amplification')
+    if gene in rearr:
+        potential_alterations.append(' rearrangement')
+    alteration_str = random.choice(potential_alterations)
+    genes_for_this_hist[i] = gene + alteration_str
 
 print genes_for_this_hist
