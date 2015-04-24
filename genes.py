cc6b29b85dd4941bd8496a1bb8276b7a608866fa
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-24 14:58:08 -0400

    pick realistic tumor histology

--- genes.py
+++ genes.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import random
+from names import quantile2text
+
+histology = quantile2text(random.uniform(0, 1), "histology.csv", 1, 0, ",")
 
 gene_list = ['ABL1', 'ERBB4', 'GNAQ', 'MTOR', 'RET', 'AKT1', 'EZH2', 'GNAS', 'NF1', 'ROS1', 'ALK', 'FANCA', 'HNF1A', 'NF2', 'SMAD4', 'APC', 'FANCC', 'HRAS', 'NOTCH1', 'SMARCB1', 'ATM', 'FANCD2', 'IDH1', 'NPM1', 'SMO', 'BRAF', 'FANCE', 'IDH2', 'NRAS', 'SRC', 'BRCA1', 'FANCF', 'JAK2', 'NTRK1', 'STK11', 'BRCA2', 'FANCG', 'JAK3', 'PALB2', 'TERT', 'BRIP1', 'FANCL', 'KDR', 'PDGFRA', 'TP53', 'CDH1', 'FBXW7', 'KIT', 'PDGFRB', 'TSC1', 'CDKN2A', 'FGFR1', 'KRAS', 'PIK3CA', 'TSC2', 'CSF1R', 'FGFR2', 'MET', 'PMS2', 'VHL', 'CTNNB1', 'FGFR3', 'MLH1', 'PTCH1', 'DDR2', 'FLT3', 'MPL', 'PTEN', 'EGFR', 'FOXL2', 'MSH2', 'PTPN11', 'ERBB2', 'GNA11', 'MSH6', 'RB1']
 
@@ -15,4 +18,5 @@ def sample_about_how_many(list, how_many):
     n = float(how_many)
     return random.sample(list, binomialvariate(len(list), n/len(list)))
 
+print histology
 print sample_about_how_many(gene_list, 3)
--- names.py
+++ names.py
@@ -3,6 +3,29 @@ import random
 import datetime
 import csv
 
+def quantile2text(quantile, filename, q_column, t_column, split_text):
+    file = open(filename, 'r')
+    found_name = ""
+    names_to_pick = []
+    matching_proportion = 0
+    while found_name == "":
+        fields = file.readline().split(split_text)
+        name = fields[t_column]
+        cumu_p = float(fields[q_column])
+        if quantile <= cumu_p and matching_proportion == 0:
+            names_to_pick.append(name)
+            matching_proportion = cumu_p
+            continue
+        elif quantile <= cumu_p and matching_proportion == cumu_p:
+            names_to_pick.append(name)
+            continue
+        elif quantile > cumu_p:
+            continue
+        else:
+            assert quantile <= cumu_p and matching_proportion < cumu_p
+            found_name = random.choice(names_to_pick).capitalize()
+    return found_name
+
 class Patient:
     def __init__(self):
         Proportion_male = 0.927
@@ -43,29 +66,6 @@ class Patient:
 
         ######## Name ########
         
-        def quantile2text(quantile, filename, q_column, t_column, split_text):
-            file = open(filename, 'r')
-            found_name = ""
-            names_to_pick = []
-            matching_proportion = 0
-            while found_name == "":
-                fields = file.readline().split(split_text)
-                name = fields[t_column]
-                cumu_p = float(fields[q_column])
-                if quantile <= cumu_p and matching_proportion == 0:
-                    names_to_pick.append(name)
-                    matching_proportion = cumu_p
-                    continue
-                elif quantile <= cumu_p and matching_proportion == cumu_p:
-                    names_to_pick.append(name)
-                    continue
-                elif quantile > cumu_p:
-                    continue
-                else:
-                    assert quantile <= cumu_p and matching_proportion < cumu_p
-                    found_name = random.choice(names_to_pick).capitalize()
-            return found_name
-
         self.fullname = []
         for filename in ('dist.all.last', firstname_file):
             r = random.uniform(0, 90.483) # names file only covers 90.5%
