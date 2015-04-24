86dd8dac5f2e5d3c586aad046568ef23304eb1d1
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-04-24 17:31:24 -0400

    Fix line wrapping

--- genes.py
+++ genes.py
@@ -16,14 +16,16 @@ class Tumor:
         #### Pick from genes with known distribution for given histology.
 
         gene_freq = {}
-        gene_freq['Adenocarcinoma'] = {'EGFR': 0.10, 'ALK': 0.10, 'ERBB2': 0.04,
-                                       'BRAF': 0.04, 'KRAS': 0.20, 'PIK3CA': 0.04,
-                                       'AKT1': 0.0, 'MAP2K1': 0.04, 'MET': 0.04}
-        gene_freq['Squamous'] = {'EGFR': 0.04, 'ALK': 0.04, 'ERBB2': 0.0,
-                                 'BRAF': 0.0, 'KRAS': 0.04, 'PIK3CA': 0.04,
-                                 'AKT1': 0.04, 'MAP2K1': 0.0, 'MET': 0.04}
-        # Pao W, Girard N. New driver mutations in non-small-cell lung cancer.
-        # Lancet Oncol. 2011 Feb;12(2):175-180. PMID: 21277552
+        gene_freq['Adenocarcinoma'] = \
+            {'EGFR': 0.10, 'ALK': 0.10, 'ERBB2': 0.04,
+             'BRAF': 0.04, 'KRAS': 0.20, 'PIK3CA': 0.04,
+             'AKT1': 0.0, 'MAP2K1': 0.04, 'MET': 0.04}
+        gene_freq['Squamous'] = \
+            {'EGFR': 0.04, 'ALK': 0.04, 'ERBB2': 0.0,
+             'BRAF': 0.0, 'KRAS': 0.04, 'PIK3CA': 0.04,
+             'AKT1': 0.04, 'MAP2K1': 0.0, 'MET': 0.04}
+        # Pao W, Girard N. New driver mutations in non-small-cell lung
+        # cancer. Lancet Oncol. 2011 Feb;12(2):175-180. PMID: 21277552
 
         self.genotype = []
 
@@ -34,9 +36,28 @@ class Tumor:
 
         #### Pick the rest of the genes
 
-        gene_set = set(['ABL1', 'ERBB4', 'GNAQ', 'MTOR', 'RET', 'AKT1', 'EZH2', 'GNAS', 'NF1', 'ROS1', 'ALK', 'FANCA', 'HNF1A', 'NF2', 'SMAD4', 'APC', 'FANCC', 'HRAS', 'NOTCH1', 'SMARCB1', 'ATM', 'FANCD2', 'IDH1', 'NPM1', 'SMO', 'BRAF', 'FANCE', 'IDH2', 'NRAS', 'SRC', 'BRCA1', 'FANCF', 'JAK2', 'NTRK1', 'STK11', 'BRCA2', 'FANCG', 'JAK3', 'PALB2', 'TERT', 'BRIP1', 'FANCL', 'KDR', 'PDGFRA', 'TP53', 'CDH1', 'FBXW7', 'KIT', 'PDGFRB', 'TSC1', 'CDKN2A', 'FGFR1', 'KRAS', 'PIK3CA', 'TSC2', 'CSF1R', 'FGFR2', 'MET', 'PMS2', 'VHL', 'CTNNB1', 'FGFR3', 'MLH1', 'PTCH1', 'DDR2', 'FLT3', 'MPL', 'PTEN', 'EGFR', 'FOXL2', 'MSH2', 'PTPN11', 'ERBB2', 'GNA11', 'MSH6', 'RB1', 'ERBB3 amplification', 'MYC amplification', 'MYCN amplification', 'ETV6 rearrangement', 'ETV1 rearrangement', 'EWSR1 rearrangement', 'TMPRSS2 rearrangement', 'BCL2 rearrangement', 'ETV4 rearrangement', 'MLL rearrangement', 'RARA rearrangement'])
+        gene_set = set(['ABL1', 'ERBB4', 'GNAQ', 'MTOR', 'RET', 'AKT1',
+                        'EZH2', 'GNAS', 'NF1', 'ROS1', 'ALK', 'FANCA',
+                        'HNF1A', 'NF2', 'SMAD4', 'APC', 'FANCC', 'HRAS',
+                        'NOTCH1', 'SMARCB1', 'ATM', 'FANCD2', 'IDH1',
+                        'NPM1', 'SMO', 'BRAF', 'FANCE', 'IDH2', 'NRAS',
+                        'SRC', 'BRCA1', 'FANCF', 'JAK2', 'NTRK1', 'STK11',
+                        'BRCA2', 'FANCG', 'JAK3', 'PALB2', 'TERT', 'BRIP1',
+                        'FANCL', 'KDR', 'PDGFRA', 'TP53', 'CDH1', 'FBXW7',
+                        'KIT', 'PDGFRB', 'TSC1', 'CDKN2A', 'FGFR1', 'KRAS',
+                        'PIK3CA', 'TSC2', 'CSF1R', 'FGFR2', 'MET', 'PMS2',
+                        'VHL', 'CTNNB1', 'FGFR3', 'MLH1', 'PTCH1', 'DDR2',
+                        'FLT3', 'MPL', 'PTEN', 'EGFR', 'FOXL2', 'MSH2',
+                        'PTPN11', 'ERBB2', 'GNA11', 'MSH6', 'RB1',
+                        'ERBB3 amplification', 'MYC amplification',
+                        'MYCN amplification', 'ETV6 rearrangement',
+                        'ETV1 rearrangement', 'EWSR1 rearrangement',
+                        'TMPRSS2 rearrangement', 'BCL2 rearrangement',
+                        'ETV4 rearrangement', 'MLL rearrangement',
+                        'RARA rearrangement'])
 
-        cnv = ['ALK', 'FGFR3', 'RET', 'EGFR', 'FGFR1', 'KIT', 'ERBB2', 'FGFR2', 'MET', 'PDGFRA']
+        cnv = ['ALK', 'FGFR3', 'RET', 'EGFR', 'FGFR1', 'KIT', 'ERBB2',
+               'FGFR2', 'MET', 'PDGFRA']
 
         rearr = ['ABL1', 'EGFR', 'PDGFRA', 'ROS1', 'ALK', 'PDGFRB']
 
