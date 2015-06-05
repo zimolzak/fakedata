40bc455ebc3f33ebe0c8c020f949ced43878479e
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-06-05 15:28:58 -0400

    Greatly reduce temp variables. Assume Python returns keys in consistent order.

--- fakedata.py
+++ fakedata.py
@@ -18,49 +18,32 @@ labs_per_patient = 20
 patients_to_generate = 30
 
 #### Initialize CSV files with headers
-test_panel = CbcBmp()
 labwriter = csv.writer(open('labs.csv', 'wb'))
-keys_ordered = public(vars(test_panel)).keys()
-labwriter.writerow(["id", "date"] + keys_ordered)
+labwriter.writerow(["id", "date"] + public(vars(CbcBmp())).keys())
 
-test_patient = Patient()
 patientwriter = csv.writer(open('patients.csv', 'wb'))
-patient_keys_ordered = vars(test_patient).keys()
-patientwriter.writerow(["id"] + patient_keys_ordered)
+patientwriter.writerow(["id"] + vars(Patient()).keys())
 
-test_tumor = Tumor()
 tumorwriter = csv.writer(open('tumor.csv', 'wb'))
-tumor_keys_ordered = vars(test_tumor).keys()
-tumorwriter.writerow(["id"] + tumor_keys_ordered)
+tumorwriter.writerow(["id"] + vars(Tumor()).keys())
 
 #### Generate patients
 
 for id in range(patients_to_generate):
 
     ### Initial conditions
-    current_person = Patient()
-    current_tumor = Tumor()
     Panel = CbcBmp()
     t = datetime.date(2014,1,1) + datetime.timedelta(random.randint(0,365))
 
     ### Write to patient table
-    vals_ordered = []
-    for k in patient_keys_ordered:
-        vals_ordered.append(current_person.__dict__[k])
-    patientwriter.writerow([id] + vals_ordered)
+    patientwriter.writerow([id] + vars(Patient()).values())
 
     ### Write to tumor table
-    vals_ordered = []
-    for k in tumor_keys_ordered:
-        vals_ordered.append(current_tumor.__dict__[k])
-    tumorwriter.writerow([id] + vals_ordered)
+    tumorwriter.writerow([id] + vars(Tumor()).values())
 
     ### Loop and write to lab table
     for i in range(labs_per_patient):
-        vals_ordered = []
-        for k in keys_ordered:
-            vals_ordered.append(vars(Panel)[k])
-        labwriter.writerow([id, str(t)] + vals_ordered)
+        labwriter.writerow([id, str(t)] + public(vars(Panel)).values())
         # The update rules are below.
         dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
         t = t + dt
