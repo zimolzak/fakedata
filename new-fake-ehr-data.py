f1c97b44043c8c6fc60296b9aa54f1001ad249af
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2024-06-27 16:03:49 -0500

    Change to pat_id.

--- new-fake-ehr-data.py
+++ new-fake-ehr-data.py
@@ -14,23 +14,24 @@ patients_to_generate = 30
 
 # Initialize CSV files with headers
 patient_writer = csv.writer(open('patients-2024.csv', 'w'))
-patient_writer.writerow(["id"] + list(public(vars(Patient())).keys()))
+patient_writer.writerow(["pat_id"] + list(public(vars(Patient())).keys()))
 
 lab_writer = csv.writer(open('labs.csv', 'w'))
 lab_names = list(public(vars(CbcBmp())).keys())
-lab_writer.writerow(["id", "date", "lab_name", "value"])
+lab_writer.writerow(["pat_id", "date", "lab_name", "value"])
 
 vis_writer = open('visits.csv', 'w')
-vis_writer.write("id,date,icd,condition\n")
+vis_writer.write("pat_id,date,icd,condition\n")
 
 med_writer = open('meds.csv', 'w')
-med_writer.write("id,date,med_prescribed\n")
+med_writer.write("pat_id,date,med_prescribed\n")
 
 # Generate patients
-for n in range(patients_to_generate):
+for pat_id in range(patients_to_generate):
+    pat_id += 1
 
     # Patient
-    patient_writer.writerow([n] + list(public(vars(Patient())).values()))
+    patient_writer.writerow([pat_id] + list(public(vars(Patient())).values()))
     lab_obj = CbcBmp(star=False)
     t = datetime.date(2014, 1, 1) + datetime.timedelta(random.randint(0, 365))
 
@@ -38,7 +39,7 @@ for n in range(patients_to_generate):
     for i in range(labs_per_patient):
         lab_values = list(public(vars(lab_obj)).values())
         for j in range(len(lab_values)):
-            lab_writer.writerow([n, str(t), lab_names[j], lab_values[j]])
+            lab_writer.writerow([pat_id, str(t), lab_names[j], lab_values[j]])
             # Update the time and the labs.
             dt = datetime.timedelta(int(random.expovariate(1.0 / avg_days)))
             t = t + dt
@@ -51,6 +52,6 @@ for n in range(patients_to_generate):
         dx_today, meds_today = p.return_one_visit()
         for r in dx_today:
             #                 ID     ,date,icd,condition\n
-            vis_writer.write(str(n) + ',' + r + '\n')
+            vis_writer.write(str(pat_id) + ',' + r + '\n')
         for r in meds_today:
-            med_writer.write(str(n) + ',' + r + '\n')
+            med_writer.write(str(pat_id) + ',' + r + '\n')
